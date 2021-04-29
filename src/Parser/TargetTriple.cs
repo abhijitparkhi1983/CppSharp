94ed476c24175d02440c4493899d553e83d1efa0
Abhijit Ashok Parkhi <abhijit.parkhi@gmail.com>
2021-04-29 23:17:55 +0530

    Update TargetTriple.cs
    Test Commit

--- src/Parser/TargetTriple.cs
+++ src/Parser/TargetTriple.cs
@@ -20,4 +20,21 @@ namespace CppSharp.Parser
                 parts.Contains("darwin") || parts.Contains("osx");
         }
     }
+    
+    public static class TargetTriple1
+    {
+        public static bool IsWindows(string targetTriple)
+        {
+            var parts = targetTriple.Split('-');
+            return parts.Contains("windows") ||
+                parts.Contains("win32") || parts.Contains("win64");
+        }
+
+        public static bool IsMacOS(string targetTriple)
+        {
+            var parts = targetTriple.Split('-');
+            return parts.Contains("apple") ||
+                parts.Contains("darwin") || parts.Contains("osx");
+        }
+    }
 }
